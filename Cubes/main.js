2fba29a30c449e1bdee4bc9bf2864dc0fea3dd89
gbelonog <gbelonog@gmail.com>
2019-09-13 20:21:32 +0300

    test

--- Cubes/main.js
+++ Cubes/main.js
@@ -1,11 +1,13 @@
 var game = document.getElementById('board');//get board
 var grid = document.createElement('section');//create grid
+var cubesBox = [0,1,1,1,0,1,1,1,0];
 grid.setAttribute('class','grid');
 game.appendChild(grid);//add grid to board
 for(i = 0; i < 9; i++)
 {
     var card = document.createElement('div');//create div element and assign it to var card
-    card.classList.add('greenState');//apply a card class to that div
+    if (cubesBox[i]==1){card.classList.add('yellowState');}//apply a green cube to that div}
+    else {card.classList.add('greenState');}//apply a yellow cube to that div
     grid.appendChild(card);//add card to grid
 };
 //add event listener to grid
