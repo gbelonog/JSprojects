2eb03c8db0fbc848832a3a8fe3f7512e690078e6
gbelonog <gbelonog@gmail.com>
2019-09-13 17:52:21 +0300

    9 in grid

--- Cubes/main.js
+++ Cubes/main.js
@@ -2,14 +2,14 @@ var game = document.getElementById('board');//get board
 var grid = document.createElement('section');//create grid
 grid.setAttribute('class','grid');
 game.appendChild(grid);//add grid to board
-for(i = 0; i < 4; i++)
+for(i = 0; i < 9; i++)
 {
     var card = document.createElement('div');//create div element and assign it to var card
-    card.classList.add('card');//apply a card class to that div
+    card.classList.add('greenState');//apply a card class to that div
     grid.appendChild(card);//add card to grid
 };
 //add event listener to grid
 grid.addEventListener('click',function(event){
     var clicked = event.target;// var for clicked item
-    clicked.classList.add('selected');
+    clicked.classList.add('yellowState');
 });
