3d3f10548c19d6168b50f9ccfb1e3d9005eddd80
galina <gbelonog@gmail.com>
2019-09-30 23:43:12 +0300

    victory

--- Cubes/main.js
+++ Cubes/main.js
@@ -1,37 +1,63 @@
 var game = document.getElementById('board');//get board
-var grid = document.createElement('grid');//create grid
-var size = 0;
-getUserChoice(prompt('Please set size of grid'));      
-var gridSize = size * 100 +size * 4;//size of grid
-grid.setAttribute('class','grid');
-grid.setAttribute('id','grid');
-game.appendChild(grid);//add grid to board
+getUserChoice(prompt('Please set size of grid')); 
 
-document.getElementById('grid').style.setProperty('--grid-width', gridSize + 'px');// set width property in css file
-document.getElementById('grid').style.setProperty('--grid-height', gridSize + 'px');// set height property in css file
-
-for(i = 0; i < (Math.pow(size,2)); i++)
-{
-    var cube = document.createElement('div');//create div element and assign it to var cube
-    var random = Math.random()+0.5;//get random digit
-    if (random > 1){
-        cube.classList.add('secondState');//apply a yellow color to cube 
-    }
-    else {cube.classList.add('firstState');}//apply a green color to cube   
-    cube.id = i;//add id to cube
-    grid.appendChild(cube);//add cube to grid
+//function checks that all cubes are in the same state
+function victoryCheck(size){
+    var counter = 0;
+    for(i = 0; i < size * size; i++){
+            //console.log(document.getElementById(0).className);
+        if(document.getElementById(i).className == 'firstState'){
+            counter++;
+        };
+    };
+    if (counter == size * size || counter == 0){
+        console.log('victory');
+        for(i = 0; i < size * size; i++){
+            document.getElementById(i).remove();
+        };
+    };
 };
-//add event listener to grid
-grid.addEventListener('click',function(event){
-    var clicked = event.target;// var for clicked item
-    changeColor(clicked);//change color of clicked element
-    selectNaibor(clicked.id);//select elements to changed with clicked
-});
+
+//function for creating grid
+function createGrid(size){
+    var grid = document.createElement('grid');//create grid      
+    var gridSize = size * 100 +size * 4;//size of grid
+    grid.setAttribute('class','grid');
+    grid.setAttribute('id','grid');
+    game.appendChild(grid);//add grid to board
+
+    document.getElementById('grid').style.setProperty('--grid-width', gridSize + 'px');// set width property in css file
+    document.getElementById('grid').style.setProperty('--grid-height', gridSize + 'px');// set height property in css file
+
+    
+
+    for(i = 0; i < (Math.pow(size,2)); i++){
+        var cube = document.createElement('div');//create div element and assign it to var cube
+        var random = Math.random()+0.5;//get random digit
+        if (random > 1){
+            cube.classList.add('secondState');//apply a yellow color to cube 
+        }
+        else {
+            cube.classList.add('firstState');//apply a green color to cube 
+        }  
+        cube.id = i;//add id to cube
+        grid.appendChild(cube);//add cube to grid
+    };
+    
+    //add event listener to grid
+    grid.addEventListener('click',function(event){
+        var clicked = event.target;// var for clicked item
+        changeColor(clicked);//change color of clicked element
+        selectNaibor(clicked.id, size);//select elements to changed with clicked
+    });
+};
 
 //function for checking user's selection
 function getUserChoice(userInput){
     if(userInput >= 0 && userInput <= 10){
-        size = userInput;}
+        //size = userInput;
+        createGrid(userInput);
+    }
     else {
         getUserChoice(prompt('Please enter a valid number'));
     }
@@ -58,7 +84,7 @@ function changeColorById(n){
     };    
     
 //function for selecting naibors of clicked element
-function selectNaibor(id){
+function selectNaibor(id, size){
     size = Number(size);
     if(id % size > 0){
         changeColorById(id - 1);
@@ -72,4 +98,5 @@ function selectNaibor(id){
     if(id > size - 1) {
         changeColorById(Number(id) - size);
     };
+    victoryCheck(size);
 };
