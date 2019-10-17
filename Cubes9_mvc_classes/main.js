73907fb0d9b69cad99ac764997bf536932afc039
gbelonog <gbelonog@gmail.com>
2019-10-17 10:39:11 +0300

    array?

--- Cubes9_mvc_classes/main.js
+++ Cubes9_mvc_classes/main.js
@@ -20,7 +20,8 @@ class CubesController{
     onClickCube(cubesModel) {
         this.cubesView.grid.addEventListener('click',function(event){
             var clicked = event.target;// var for clicked item
-            cubesModel.cubesArray[clicked.id] = 1;
+            //cubesModel.cubesArray[clicked.id] = 1;
+            cubesModel.changeColorOfClickedCube(clicked.id);
             cubesModel.drow();
        });    
     };
@@ -34,43 +35,64 @@ class CubesModel{
         this.size = 0;
         this.cubesView = cubesView;
         this.flag = false;
-    }
+    };
+
     createGrid(){
         for(var i = 0; i < this.size * this.size; i++){
             this.cubesArray[i] = 0;    
         };
         this.emulateUserClicks();
         this.drow();
-        //this.cubesView.removeGrid();
     };
+    
     drow(){
-        //console.log(this.cubesArray);
-        //this.cubesView.removeGrid();
-        //var flag = false;
-        if (this.flag == true)
-        {this.cubesView.removeGrid()};
-
+        if (this.flag == true){this.cubesView.removeGrid()};
         this.cubesView.drowGrid(this.cubesArray);
         this.flag = true;
     };
 
-    //createGrid();
-    //emulateUserClicks(size);
-    //drowGrid(cubesArray);
     emulateUserClicks(){
         var firstRandomId = Math.floor(Math.random()*100) % (Math.pow(Number(this.size),2));
         var secondRandomId = Math.floor(firstRandomId / 2);
         console.log(firstRandomId);
         console.log(secondRandomId);
-
         this.cubesArray[firstRandomId] = 1;
         this.cubesArray[secondRandomId] = 1;
     }; 
+    
     //function to remove all cubes from grid
     removeCubesFromGrid(){
         this.cubesArray = [];   
     };
     
+    //change color of clicked cube
+    changeColorOfClickedCube(id){
+        if(this.cubesArray[id] == 1){this.cubesArray[id] = 0;}
+        else {this.cubesArray[id] = 1;}
+        this.selectNeighbour(id, this.cubesView);
+    };
+    //function for selecting neighbours of clicked element
+    selectNeighbour(id, cubesView){
+        var size = Number(this.size);
+        var x = document.getElementById(id).getAttribute('x');
+        var y = document.getElementById(id).getAttribute('y');
+        if(x < size - 1){
+            cubesView.changeColor(document.querySelector('[x='+ '"' + (Number(x)+1) + '"'+'][y='+ '"' + y + '"'+']'));
+        };
+        if(x > 0){
+            cubesView.changeColor(document.querySelector('[x='+ '"' + (Number(x)-1) + '"'+'][y='+ '"' + y + '"'+']'));
+        };
+        if(y < size - 1){
+            cubesView.changeColor(document.querySelector('[x='+ '"' + x + '"'+'][y='+ '"' + (Number(y)+1) + '"'+']'));
+        };
+        if(y > 0){
+            cubesView.changeColor(document.querySelector('[x='+ '"' + x + '"'+'][y='+ '"' + (Number(y)-1) + '"'+']'));
+        };    
+        //victoryCheck(size);
+        };
+    //createGrid();
+    //emulateUserClicks(size);
+    //drowGrid(cubesArray);
    /* getUserChoice(userInput){
         if(userInput > 1 && userInput < 10){
 
@@ -101,8 +123,7 @@ class CubesView{
         //removeCubesFromGrid(size);
         
         this.addCubesToGrid(cubesArray, this.grid, size);//add cubes 
-        //console.log(grid);
-    //};
+        //};
        /* grid.addEventListener('click',function(event){
             var clicked = event.target;// var for clicked item
             console.log(clicked.id);
@@ -113,9 +134,7 @@ class CubesView{
         });*/
     };
     removeGrid(){
-        //document.getElementById('grid').remove();
         document.getElementById('grid').remove();
-        //element.removeChild(element);
     };
 
     addCubesToGrid(cubesArray, grid, size){
@@ -139,32 +158,37 @@ class CubesView{
     };
          
 //function to remove all cubes from grid
-removeCubesFromGrid(size){
+/*removeCubesFromGrid(size){
     for(var i = 0; i < size * size; i++){
         //console.log(document.getElementById(i));
         if (document.getElementById(i) > 0){
             document.getElementById(i).remove();//removing cubes
         };
     };    
-};
-};
-
-/*CubesController.start = function start() {
-    //this.penguinView.onClickGetPenguin = this.onClickGetPenguin.bind(this);
-    //this.cubesModel.cubesArray = [];
-    this.cubesModel.createGrid('0');
 };*/
+    changeColor(cube){
+        if (cube.classList.contains('grid')){
+            return 0;
+        };
+        if (cube.classList.contains('secondState')){
+            cube.classList.remove('secondState');
+            cube.classList.add('firstState');
+        }
+        else {
+            cube.classList.remove('firstState');
+            cube.classList.add('secondState');
+        }
+    };
+};
 
 
+let myGameView = new CubesView();
+let myGameModel  = new CubesModel(myGameView);
+let myGameController = new CubesController(myGameView, myGameModel);
+myGameController.start();
 
-    /*grid.addEventListener('click',function(event){
-        var clicked = event.target;// var for clicked item
-        controller('click', clicked.id);
-       // changeColor(clicked);//change color of clicked element
-       // selectNeighbour(clicked.id, size);//select elements to changed with clicked
-    });*/
-
-    //function for checking user's selection
+/*
+//function for checking user's selection
     function getUserChoice(userInput){
         if(userInput > 1 && userInput < 10){
           //createGrid(userInput);
@@ -175,34 +199,6 @@ removeCubesFromGrid(size){
         }   
     } 
 //};
-
-
-//--------model----------
-function cubesModel(size){
-    var cubesArray = [];
-    
-    createGrid();
-    emulateUserClicks(size);
-    drowGrid(cubesArray);
-    //controller(grid);
-
-
-    function createGrid(){
-        for(var i = 0; i < size * size; i++){
-            cubesArray[i] = 0;
-        };
-    };
-
-    function emulateUserClicks(size){
-        var firstRandomId = Math.floor(Math.random()*100) % (Math.pow(Number(size),2));
-        var secondRandomId = Math.floor(firstRandomId / 2);
-        console.log(firstRandomId);
-        console.log(secondRandomId);
-
-        cubesArray[firstRandomId] = 1;
-        cubesArray[secondRandomId] = 1;
-    };
-
 
 //function checks that all cubes are in the same state
 /*function victoryCheck(size){
@@ -297,58 +293,8 @@ function random(size){
     };    
     victoryCheck(size);
 };*/
-};
-//------------view--------------
-function drowGrid(cubesArray){
-    var size = cubesArray.length / 2;
-    var game = document.getElementById('board');//get board
-    var grid = document.createElement('grid');//create grid 
-    var gridSize = size * 100 +size * 4;
-    grid.setAttribute('class','grid');
-    grid.setAttribute('id','grid');
-    game.appendChild(grid);//add grid to board
-    document.getElementById('grid').style.setProperty('--grid-width', gridSize + 'px');// set width property in css file
-    document.getElementById('grid').style.setProperty('--grid-height', gridSize + 'px');// set height property in css file
-    //removeCubesFromGrid(size);
-    addCubesToGrid(grid, size);//add cubes 
-    //console.log(grid);
-    
-    /*grid.addEventListener('click',function(event){
-        var clicked = event.target;// var for clicked item
-        controller('click', clicked.id);
-       // changeColor(clicked);//change color of clicked element
-       // selectNeighbour(clicked.id, size);//select elements to changed with clicked
-    });*/
 
-    function addCubesToGrid(grid, size){
-        var i = 0;
-        for(var y = 0; y < size; y++){
-            for(var x = 0; x < size; x++){
-                var cube = document.createElement('cube');//create div element and assign it to var cube
-                if (cubesArray[i] == 0){
-                    cube.classList.add('firstState');//apply a green color to cube
-                }
-                else{
-                    cube.classList.add('secondState');//apply a green color to cube
-                } 
-                cube.id = i++;
-                cube.setAttribute('x',x);//add x to cube
-                cube.setAttribute('y',y);//add y to cube
-                grid.appendChild(cube);//add cube to grid            
-            };
-        };
-        //emulateUserClicks(size);
-    };
-         
-//function to remove all cubes from grid
-function removeCubesFromGrid(size){
-    for(var i = 0; i < size * size; i++){
-        //console.log(document.getElementById(i));
-        if (document.getElementById(i) > 0){
-        document.getElementById(i).remove();//removing cubes
-        };
-    };    
-};
+
    /* function changeColor(cube){
         if (cube.classList.contains('grid')){
             return 0;
@@ -363,10 +309,7 @@ function removeCubesFromGrid(size){
         }
     };*/
 
-};
-let myGameView = new CubesView();
-let myGameModel  = new CubesModel(myGameView);
-let myGameController = new CubesController(myGameView, myGameModel);
-myGameController.start();
+
+
 
 
