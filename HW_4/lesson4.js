6dc45c0a96db5b99b8c39008c9d14dbd92de4408
galina <gbelonog@gmail.com>
2019-11-19 00:00:20 +0200

    lesson4

---
+++ HW_4/lesson4.js
@@ -0,0 +1,19 @@
+'use strict';
+function square(){
+    let input = prompt("?");
+    for(let i = 0; i < Math.sqrt(input); i++){
+        console.log(i);
+    }
+};
+function simple(){
+    let input = prompt("?");
+    if(input > 0){
+        for (let i = 0; i < 10; i++){
+            if 
+        }
+    }
+        console.log(i);
+    }
+};
+
+square();
