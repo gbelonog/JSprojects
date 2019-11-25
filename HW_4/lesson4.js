76eb578fe491dc56c352629c47cc7c5330bbeef4
gbelonog <gbelonog@gmail.com>
2019-11-25 17:45:50 +0200

    lesson4

--- HW_4/lesson4.js
+++ HW_4/lesson4.js
@@ -1,32 +1,45 @@
 'use strict';
 function square(){
-    let input = prompt("?");
+    let input = prompt("nuber for square?");
     for(let i = 0; i < Math.sqrt(input); i++){
         console.log(i);
     }
 };
-function simple(){
-    let input = prompt("?");
+function isItSimple(){
+    let input = prompt("is it simpe?");
     let simpleFlag = false;
-    if(input > 0){
+    if(input < 0){
+        simpleFlag = false;   
+    }
+    else if (input == 1 || input == 2){
+        simpleFlag = true;
+    } else {
         for (let i = 2; i < input; i++){
-            if (input % i > 0){
+            if (input % i == 0){
                 simpleFlag = false;
-                
-            }else {
-                simpleFlag = true;
-                break;
+                break;    
+            }else{
+                simpleFlag = true; 
             }
         }
-    }
-    else  simpleFlag = false;
-
+    };
     if (simpleFlag){
-        console.log("not simple");
-    }else console.log("simple");
-    //console.log("negative");
-   
+        console.log("simple");
+    }else console.log("not simple");
 };
 
+function isItMinOrMax(){
+    let input = [];
+    input[0] = prompt("Enter first digit");
+    input[1] = prompt("Enter second digit");
+    input[2] = prompt("Enter third digit");
+    let minOrMax = prompt("Enter + for max and - for min");
+    console.log(input);
+    let sortedInput = input.sort();
+    if (minOrMax == '+'){console.log('max is '+ sortedInput[2]);}
+    else{console.log('min is '+ sortedInput[0]);}
+}
+
 //square();
-simple();
+//isItSimple();
+//isItMinOrMax();
